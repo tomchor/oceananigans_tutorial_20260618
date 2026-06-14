1045cc1aa0ff610e11ca41a3bcf3342365087523
tomaschor <tomaschor@gmail.com>
2026-06-13 19:00:10 -0700

    fix plotting for bariclinic adjustment

--- baroclinic_adjustment/plot_baroclinic_adjustment.jl
+++ baroclinic_adjustment/plot_baroclinic_adjustment.jl
@@ -1,4 +1,4 @@
-using Oceananigans
+using NCDatasets
 using GLMakie
 using Printf
 using Statistics: quantile
@@ -6,23 +6,41 @@ using Statistics: quantile
 # =============================================================================
 # Animate baroclinic_adjustment.jl output.
 # Run baroclinic_adjustment.jl first to produce the .nc files.
+#
+# We read the NetCDF output directly with NCDatasets rather than through
+# Oceananigans' FieldTimeSeries. The surface fields were saved at a single
+# z-level and the zonal-mean diagnostics were written with Average(…, dims=1)
+# (a reduced x-dimension); the NetCDF FieldTimeSeries reader in this
+# Oceananigans version does not reconstruct either of those cleanly.
 # =============================================================================
 
-# --- Load timeseries ---
-b_ts = FieldTimeSeries("baroclinic_adjustment_surface.nc", "b")
-ζ_ts = FieldTimeSeries("baroclinic_adjustment_surface.nc", "ζ")
-B_ts = FieldTimeSeries("baroclinic_adjustment_zonal.nc",   "b")
-U_ts = FieldTimeSeries("baroclinic_adjustment_zonal.nc",   "u")
-V_ts = FieldTimeSeries("baroclinic_adjustment_zonal.nc",   "v")
+# --- Surface fields: (x, y) slices at the top level, stored as (x, y, z=1, t) ---
+ds_surface = NCDataset("baroclinic_adjustment_surface.nc")
+xc = ds_surface["x_caa"][:]           # x cell centers (b)
+yc = ds_surface["y_aca"][:]           # y cell centers (b)
+xf = ds_surface["x_faa"][:]           # x cell faces   (ζ)
+yf = ds_surface["y_afa"][:]           # y cell faces   (ζ)
+b_data = ds_surface["b"][:, :, 1, :]  # (x, y, time)
+ζ_data = ds_surface["ζ"][:, :, 1, :]  # (x, y, time)
+times  = ds_surface["time"][:]        # seconds
+close(ds_surface)
 
-times = b_ts.times
-Nt    = length(times)
+# --- Zonal-mean fields: (y, z) sections, stored as (y, z, t) ---
+ds_zonal = NCDataset("baroclinic_adjustment_zonal.nc")
+y_centers = ds_zonal["y_aca"][:]      # y cell centers (u)
+y_faces   = ds_zonal["y_afa"][:]      # y cell faces   (v)
+z_centers = ds_zonal["z_aac"][:]      # z cell centers
+U_data = ds_zonal["u"][:, :, :]       # (y, z, time)
+V_data = ds_zonal["v"][:, :, :]       # (y, z, time)
+close(ds_zonal)
+
+Nt = length(times)
 
 # --- Colormap limits ---
-b_min, b_max = extrema(interior(b_ts))
-ζ_lim        = max(quantile(abs.(vec(interior(ζ_ts))), 0.99), eps())
-U_lim        = max(quantile(abs.(vec(interior(U_ts))), 0.99), eps())
-V_lim        = max(quantile(abs.(vec(interior(V_ts))), 0.99), eps())
+b_min, b_max = extrema(b_data)
+ζ_lim        = max(quantile(abs.(vec(ζ_data)), 0.99), eps())
+U_lim        = max(quantile(abs.(vec(U_data)), 0.99), eps())
+V_lim        = max(quantile(abs.(vec(V_data)), 0.99), eps())
 
 # --- Figure layout ---
 fig = Figure(size=(1300, 900))
@@ -39,15 +57,15 @@ ax_ζ = Axis(fig[1, 3]; title="Surface vorticity  ζ = ∂ₓv − ∂ᵧu",   k
 ax_U = Axis(fig[2, 1]; title="Zonal-mean zonal velocity  ⟨u⟩",      kwargs_yz...)
 ax_V = Axis(fig[2, 3]; title="Zonal-mean meridional velocity  ⟨v⟩", kwargs_yz...)
 
-b_plt = @lift view(b_ts[$n], :, :, 1)
-ζ_plt = @lift view(ζ_ts[$n], :, :, 1)
-U_plt = @lift view(U_ts[$n], 1, :, :)
-V_plt = @lift view(V_ts[$n], 1, :, :)
+b_plt = @lift view(b_data, :, :, $n)
+ζ_plt = @lift view(ζ_data, :, :, $n)
+U_plt = @lift view(U_data, :, :, $n)
+V_plt = @lift view(V_data, :, :, $n)
 
-hm_b = heatmap!(ax_b, b_plt; colormap=:thermal, colorrange=(b_min, b_max))
-hm_ζ = heatmap!(ax_ζ, ζ_plt; colormap=:balance, colorrange=(-ζ_lim, ζ_lim))
-hm_U = heatmap!(ax_U, U_plt; colormap=:balance, colorrange=(-U_lim, U_lim))
-hm_V = heatmap!(ax_V, V_plt; colormap=:balance, colorrange=(-V_lim, V_lim))
+hm_b = heatmap!(ax_b, xc, yc, b_plt; colormap=:thermal, colorrange=(b_min, b_max))
+hm_ζ = heatmap!(ax_ζ, xf, yf, ζ_plt; colormap=:balance, colorrange=(-ζ_lim, ζ_lim))
+hm_U = heatmap!(ax_U, y_centers, z_centers, U_plt; colormap=:balance, colorrange=(-U_lim, U_lim))
+hm_V = heatmap!(ax_V, y_faces,   z_centers, V_plt; colormap=:balance, colorrange=(-V_lim, V_lim))
 
 Colorbar(fig[1, 2], hm_b; label="b (m s⁻²)")
 Colorbar(fig[1, 4], hm_ζ; label="ζ (s⁻¹)")
